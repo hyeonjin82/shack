5c52681138854cb3348905007a61d4050f21fc3d
Park, Alex <alexpark90@hotmail.com>
2016-05-20 17:58:11 -0400

    finsihed the basic config

--- app/app.js
+++ app/app.js
@@ -1,11 +1,56 @@
-'use strict';
+(function() {
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-    'ngRoute',
-    'ui.router',
-    'ngMaterial'
-]).
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'use strict';
+    
+    var shack = angular.module('shack', [
+        'ngRoute',
+        'ui.router',
+        'ngMaterial',
+        'ngAnimate'
+    ]);
+
+    // material design config
+    shack.config(["$mdThemingProvider", "$mdIconProvider", function ($mdThemingProvider, $mdIconProvider){
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('indigo')
+            .accentPalette('pink');
+
+    }]);
+
+    // ui-router config
+    shack.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider){
+
+          // For any unmatched url, redirect to /main
+          $urlRouterProvider.otherwise("/main");
+          
+          // Set up the states
+          $stateProvider
+            .state('main', {
+              url: "/main",
+              templateUrl: "main/main.html",
+              controller: 'MainController'
+            })
+            .state('car_detail', {
+              url: "/car_detail/:id",
+              templateUrl: "car_detail/car_detail.html",
+              controller: 'DetailController'
+            })
+            .state('account', {
+              url: "/account",
+              templateUrl: "account/account.html",
+              controller: 'AccountController'
+            })
+            .state('login', {
+              url: "/login",
+              templateUrl: "user/login/login.html",
+              controller: 'LoginController'
+            })
+            .state('signup', {
+              url: "/signup",
+              templateUrl: "user/signup/signup.html",
+              controller: 'SignupController'
+            })
+        }]);
+})();
+
